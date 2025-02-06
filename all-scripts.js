fab7223e4481261cb4c2dd74162da6ef70212e8d
almhtwy1 <94498874+almhtwy1@users.noreply.github.com>
2025-02-06 14:09:52 +0300

    Update all-scripts.js

--- all-scripts.js
+++ all-scripts.js
@@ -1,14 +1,14 @@
 (async function() {
     const scripts = [
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/titles%20Cache.js',
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/Khamsat%20Categories.js',
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/requests.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Khamsat_Categories.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Khamsat_Contact_and_Rating.js',
         'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Khamsat_Request_Expander.js',
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/Price%20Analysis%20Card.js',
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/Notes_Toggle.js',
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/Khamsat%20Contact%20%26%20Rating.js',
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/comment%20Text.js',
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/Smart%20responses.js'
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Notes_Toggle.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Price_Analysis_Card.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Smart_responses.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/comment_Text.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/requests.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/titles_Cache.js'
     ];
 
     for (const url of scripts) {
