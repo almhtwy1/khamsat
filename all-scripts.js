79152797a3be92dbee8c6dbd9614fe45f16ecd7b
almhtwy1 <94498874+almhtwy1@users.noreply.github.com>
2025-02-06 15:50:36 +0300

    Update all-scripts.js

--- all-scripts.js
+++ all-scripts.js
@@ -1,8 +1,8 @@
 // تحميل جميع السكريبتات المطلوبة
 (async function loadScripts() {
     const scripts = [
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/titles_Cache.js',
-        'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Khamsat_Categories.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/titles_Cache.js',
+        'https://raw.githubusercontent.com/almhtwy1/khamsat/main/Khamsat_Categories.js',
         'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/requests.js',
         'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Price_Analysis_Card.js',
         'https://raw.githubusercontent.com/almhtwy1/khamsat/refs/heads/main/Notes_Toggle.js',
